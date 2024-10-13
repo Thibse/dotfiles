1af005b14fdcb22092ea65a72902dba9b663186a
Thibse <thibault.rey@outlook.com>
2024-10-13 18:28:34 +0200

    ✨ add bootstrap sh
    - install required apps
    - copy dotfiles to home directory

---
+++ bootstrap.sh
@@ -0,0 +1,87 @@
+#!/usr/bin/env bash
+
+isPackageInstalled() {
+  if [ -n "$(command -v $1)" ]; then
+    return 0
+  else
+    return 1
+  fi
+}
+
+installRequired() {
+  if [ -f /etc/os-release ]; then
+    source /etc/os-release
+
+    if [ "$ID" == "ubuntu" ]; then
+      echo "Detected Ubuntu."
+      package_manager="apt"
+    elif [ "$ID" == "arch" ]; then
+      echo "Detected Arch Linux."
+      package_manager="pacman"
+    else
+      echo "Unsupported distribution. Cannot install packages."
+      exit 1
+    fi
+  fi
+
+  if ! isPackageInstalled zsh; then
+    echo "Zsh is not installed. Installing..."
+    if [ "$package_manager" == "apt" ]; then
+      sudo apt update
+      sudo apt install zsh -y
+    elif [ "$package_manager" == "pacman" ]; then
+        sudo pacman -Sy zsh --noconfirm
+    fi
+  fi
+
+  if ! isPackageInstalled nvim; then
+    echo "Neovim is not installed. Installing..."
+    if [ "$package_manager" == "apt" ]; then
+      sudo apt update
+      sudo apt install fd-find ripgrep -y
+
+      wget https://github.com/neovim/neovim/releases/download/stable/nvim-linux64.tar.gz --no-verbose
+
+      tar -xzf nvim-linux64.tar.gz
+      sudo cp -r nvim-linux64/* /usr
+    elif [ "$package_manager" == "pacman" ]; then
+      sudo pacman -Sy neovim ripgrep --noconfirm
+    fi
+  fi
+
+  if ! isPackageInstalled tmux; then
+    echo "Tmux is not installed. Installing..."
+    if [ "$package_manager" == "apt" ]; then
+      sudo apt update
+      sudo apt install tmux -y
+    elif [ "$package_manager" == "pacman" ]; then
+      sudo pacman -Sy tmux --noconfirm
+    fi
+  fi
+
+  if ! isPackageInstalled rsync; then
+    echo "Rsync is not installed. Installing..."
+    if [ "$package_manager" == "apt" ]; then
+      sudo apt install rsync -y
+    elif [ "$package_manager" == "pacman" ]; then
+      sudo pacman -Sy rsync --noconfirm
+    fi
+  fi
+}
+
+copyToHome() {
+  rsync --exclude ".git/" \
+    --exclude "bootstrap.sh" \
+    --exclude "README.md" \
+    --exclude "nvim-linux64" \
+    -avh --no-perms . ~;
+}
+
+installRequired
+copyToHome
+
+echo "Sync neovim lazy packages..."
+nvim --headless "+Lazy! sync" +qa
+echo "Synced neovim lazy."
+
+sudo chsh -s "$(command -v zsh)" "$USER"
