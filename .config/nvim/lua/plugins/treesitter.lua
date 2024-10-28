dc8991dc4e925bd688cdeec10bf8c6299c6f0d0b
Thibault Rey <thibault.rey@outlook.com>
2024-10-28 08:19:28 +0000

    ✨ neovim treesitter add vue + (s)css

--- .config/nvim/lua/plugins/treesitter.lua
+++ .config/nvim/lua/plugins/treesitter.lua
@@ -7,7 +7,7 @@ return {
     build = ':TSUpdate',
     config = function()
         require('nvim-treesitter.configs').setup({
-            ensure_installed = { 'c', 'lua', 'python', 'tsx', 'typescript', 'vimdoc', 'vim', 'markdown' },
+            ensure_installed = { 'c', 'lua', 'python', 'tsx', 'typescript', 'vimdoc', 'vim', 'markdown', 'vue', 'css', 'scss' },
 
             auto_install = false,
             highlight = { enable = true },
