e6aa1446dc4a6ed025c439c38e320fe92dd365c4
Thibault Rey <thibault.rey@outlook.com>
2025-03-01 13:59:32 +0000

    ✨ nvim add telescope git_branches mapping

--- .config/nvim/lua/plugins/telescope.lua
+++ .config/nvim/lua/plugins/telescope.lua
@@ -17,5 +17,7 @@ return {
 		vim.keymap.set("n", "<leader>sr", builtin.resume, { desc = "[S]earch [R]esume" })
 		vim.keymap.set("n", "<leader>s.", builtin.oldfiles, { desc = '[S]earch Recent Files ("." for repeat)' })
 		vim.keymap.set("n", "<leader><leader>", builtin.buffers, { desc = "[ ] Find existing buffers" })
+
+		vim.keymap.set("n", "<leader>gc", builtin.git_branches, { desc = "[G]it [C]heckout" })
 	end,
 }
