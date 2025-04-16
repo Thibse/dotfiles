38a1e77988cd6758c616ed58f7ba91f4d0ab4e2b
Thibault Rey <66947803+Thibse@users.noreply.github.com>
2025-04-16 09:02:54 +0200

    ✨ nvim: add quickfix keymaps
    Map quickfix list next + previous.

--- .config/nvim/lua/config/remap.lua
+++ .config/nvim/lua/config/remap.lua
@@ -8,3 +8,6 @@ vim.keymap.set({"n", "v"}, "<leader>y", [["+y]])
 vim.keymap.set({"n", "v"}, "<leader>p", [["+p]])
 
 vim.keymap.set({ 'n', 'v' }, '<Space>', '<Nop>', { silent = true })
+
+vim.keymap.set("n", "<M-j>", "<cmd>cnext<CR>")
+vim.keymap.set("n", "<M-k>", "<cmd>cprev<CR>")
