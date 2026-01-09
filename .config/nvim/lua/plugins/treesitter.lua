195f1cf6bdbf74c5f839f00893b23ab54f320bb4
Thibault Rey <66947803+Thibse@users.noreply.github.com>
2026-01-09 11:38:44 +0100

    🔧 nvim: update treesitter setup

--- .config/nvim/lua/plugins/treesitter.lua
+++ .config/nvim/lua/plugins/treesitter.lua
@@ -2,17 +2,15 @@ return {
   {
     'nvim-treesitter/nvim-treesitter',
     dependencies = {
-      'nvim-treesitter/nvim-treesitter-textobjects',
+      {
+        'nvim-treesitter/nvim-treesitter-textobjects',
+        branch = "main"
+      },
     },
+    lazy = false,
     build = ':TSUpdate',
     config = function()
-        require('nvim-treesitter.configs').setup({
-            ensure_installed = { 'c', 'lua', 'python', 'tsx', 'typescript', 'vimdoc', 'vim', 'markdown', 'vue', 'css', 'scss', 'c_sharp' },
-
-            auto_install = false,
-            highlight = { enable = true },
-            indent = { enable = true }
-        })
+        require('nvim-treesitter').install = { 'c', 'lua', 'python', 'tsx', 'typescript', 'vimdoc', 'vim', 'markdown', 'vue', 'css', 'scss', 'c_sharp' }
     end
   },
 }
